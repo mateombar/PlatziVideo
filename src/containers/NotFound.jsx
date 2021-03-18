9aa88229ebd4f46ce98365a4814610b4410fff89
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-17 19:40:33 -0500

    Notfound container

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/NotFound.scss";
+const NotFound = () => (
+  <div className="NotFound">
+    <div className="container">
+      <h1>Ups</h1>
+      <h4>La pagina que buscas no está disponible</h4>
+    </div>
+  </div>
+);
+
+export default NotFound;
