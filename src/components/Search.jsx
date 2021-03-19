887299bd5e4737f3aecf9319771b2a17104e62ab
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-19 08:02:14 -0500

    UI Validation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import '../assets/styles/components/Search.scss';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "../assets/styles/components/Search.scss";
 
-const Search = () => (
-  <section className="main">
-    <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-    <input type="text" className="input" placeholder="Buscar..." />
-  </section>
-);
+const Search = () => {
+  const location = useLocation();
+  const inputStyle = location.pathname === "/" ? "input-home" : "";
+  return (
+    <section className="main">
+      <h2 className="main__title">¿Qué quieres ver hoy?</h2>
+      <input type="text" className={`input ${inputStyle}`} placeholder="Buscar..." />
+    </section>
+  );
+};
 
 export default Search;
