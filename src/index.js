3afd24d39e7919348ce1212ada901fd6a1531cc9
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-19 09:03:35 -0500

    Implementing redux dev tools with compose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducer from './reducers/index.js';
 import App from './routes/App';
@@ -173,10 +173,13 @@ const initialState = {
     ]
 }
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-    reducer, 
+    reducer,
     initialState,
-    applyMiddleware(reduxThunk));
+    composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
